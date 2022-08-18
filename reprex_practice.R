1d828440a9e958ac1506edff8cfda3dd0b005b93
romero61 <bsf310@gmail.com>
2022-08-18 15:11:17 -0700

    tabels with kable

--- reprex_practice.R
+++ reprex_practice.R
@@ -1,5 +1,6 @@
 #---- Practicing Reprex #---
 
+# Not A Reprex
 library(tidyverse)
 library(palmerpenguins)
 
@@ -11,3 +12,28 @@ penguins |>
   summarize(mean(body_mass_g, na.rm = TRUE),
             mean(flipper_length_mm, na.rm = TRUE))
 
+### A REPREX
+library(tidyverse)
+
+warpbreaks |> 
+  str_to_lower(wool)
+
+### A Fixing Reprex
+library(tidyverse)
+
+warpbreaks |> 
+  mutate(wool = str_to_lower(wool))
+
+
+### REPREX WITH A SYNTHESIZED DATA FRAME
+library(tidyverse)
+
+upper_case <- tribble(
+  ~letter,
+  'A',
+  'B',
+  'C'
+)
+
+upper_case |> 
+  str_to_lower(letter)
