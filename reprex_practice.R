25bd3d79e48b17578a6b8ebe6494fe627d2f5b0c
romero61 <bsf310@gmail.com>
2022-08-18 14:23:32 -0700

    Making a reprex

--- reprex_practice.R
+++ reprex_practice.R
@@ -1,4 +1,13 @@
 #---- Practicing Reprex #---
 
+library(tidyverse)
+library(palmerpenguins)
 
+penguins |> 
+  select(species, body_mass_g, flipper_length_mm, year) |> 
+  filter(species == "Chinstrap") |> 
+  str_to_lower(species) |> 
+  group_by(island) |> 
+  summarize(mean(body_mass_g, na.rm = TRUE),
+            mean(flipper_length_mm, na.rm = TRUE))
 
